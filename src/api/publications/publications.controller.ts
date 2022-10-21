540dec6a6d9450ca85a94d0de9baced704091855
David Pineda <dspinedao@gmail.com>
2022-10-21 05:34:56 -0500

    feat/add bearer token all routes

--- src/api/publications/publications.controller.ts
+++ src/api/publications/publications.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 
 export async function publications(req: Request, res: Response) {
   const { BASE_URL } = process.env;
+  const auth = req.headers?.authorization;
+  const token = auth?.split(" ")[1];
   const getPosts = await fetch("https://jsonplaceholder.typicode.com/posts");
   const postsJson = await getPosts.json();
   const dataRegister = { data: postsJson, method: "GET POSTS" };
@@ -9,6 +11,7 @@ export async function publications(req: Request, res: Response) {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(dataRegister),
   });
